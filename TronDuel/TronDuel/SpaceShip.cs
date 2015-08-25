eb831029aefd59c25164a50d87b5124da586311e
mihail-stefanov <mihail-stefanov@users.noreply.github.com>
2015-08-26 01:32:33 +0300

    Created initial version of the game. Created class SpaceShip.cs, enumeration Direction.cs and method MoveShip.

---
+++ TronDuel/TronDuel/SpaceShip.cs
@@ -0,0 +1,19 @@
+﻿namespace TronDuel
+{
+    public class SpaceShip
+    {
+        public const char ShipCharRight = '►';
+        public const char ShipCharLeft = '◄';
+        public const char ShipCharUp = '▲';
+        public const char ShipCharDown = '▼';
+
+        public SpaceShip(byte startingPositionX, byte startingPositionY)
+        {
+            this.Xposition = startingPositionX;
+            this.Yposition = startingPositionY;
+        }
+
+        public byte Xposition { get; set; }
+        public byte Yposition { get; set; }
+    }
+}
---
+++ TronDuel/TronDuel/TronDuelMain.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TronDuel
+{
+    public class TronDuelMain
+    {
+        static void Main()
+        {
+            Console.BufferHeight = Console.WindowHeight = 30;
+            Console.BufferWidth = Console.WindowWidth = 70;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.CursorVisible = false;
+
+            SpaceShip spaceShipPlayerOne = new SpaceShip(10, 10);
+
+            Console.SetCursorPosition(spaceShipPlayerOne.Xposition, spaceShipPlayerOne.Yposition);
+            Console.Write(SpaceShip.ShipCharRight);
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo pressedKey = Console.ReadKey();
+
+                    if (pressedKey.Key == ConsoleKey.RightArrow)
+                    {
+                        MoveShip(spaceShipPlayerOne, Direction.Right);
+                    }
+                    else if (pressedKey.Key == ConsoleKey.LeftArrow)
+                    {
+                        MoveShip(spaceShipPlayerOne, Direction.Left);
+                    }
+                    else if (pressedKey.Key == ConsoleKey.UpArrow)
+                    {
+                        MoveShip(spaceShipPlayerOne, Direction.Up);
+                    }
+                    else if (pressedKey.Key == ConsoleKey.DownArrow)
+                    {
+                        MoveShip(spaceShipPlayerOne, Direction.Down);
+                    }
+                }
+            }
+        }
+
+        private static void MoveShip(SpaceShip spaceShipPlayerOne, Direction direction)
+        {
+            Console.SetCursorPosition(spaceShipPlayerOne.Xposition, spaceShipPlayerOne.Yposition);
+            Console.Write(' ');
+            switch (direction)
+            {
+                case Direction.Right:
+                    spaceShipPlayerOne.Xposition++;
+                    Console.SetCursorPosition(spaceShipPlayerOne.Xposition, spaceShipPlayerOne.Yposition);
+                    Console.Write(SpaceShip.ShipCharRight);
+                    break;
+                case Direction.Left:
+                    spaceShipPlayerOne.Xposition--;
+                    Console.SetCursorPosition(spaceShipPlayerOne.Xposition, spaceShipPlayerOne.Yposition);
+                    Console.Write(SpaceShip.ShipCharLeft);
+                    break;
+                case Direction.Up:
+                    spaceShipPlayerOne.Yposition--;
+                    Console.SetCursorPosition(spaceShipPlayerOne.Xposition, spaceShipPlayerOne.Yposition);
+                    Console.Write(SpaceShip.ShipCharUp);
+                    break;
+                case Direction.Down:
+                    spaceShipPlayerOne.Yposition++;
+                    Console.SetCursorPosition(spaceShipPlayerOne.Xposition, spaceShipPlayerOne.Yposition);
+                    Console.Write(SpaceShip.ShipCharDown);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
